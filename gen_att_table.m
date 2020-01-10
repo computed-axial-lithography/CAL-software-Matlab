1899f8e0d1726f50a5f0070ab0cfa5146ff46e24
jttoombs <38547166+jttoombs@users.noreply.github.com>
2020-01-09 18:13:06 -0800

    ...

--- gen_att_table.m
+++ gen_att_table.m
@@ -1,4 +1,16 @@
 function att_table = gen_att_table(params,domain_size)
+% INPUTS:  params        =  struct, contains all parameters specific to the
+%                           process including vial radius, pentration depth,
+%                           interpolation method
+%          domain_size   =  vector, size of the reconstruction/target space
+% 
+% OUTPUTS: att_table     =  matrix, [NxNxN_theta] contribution of resin 
+%                           attenuation at each angle in params.theta; 
+%                           each contribution is used as a multiplier
+%                           modifying the backprojected intensity in
+%                           exp_iradon()
+
+
 
 % Preallocate 3D matrix for lookup table
 att_table = single(zeros([domain_size,length(params.theta)]));
@@ -19,7 +31,7 @@ ytop = center - 1;
 y = (N:-1:1).' - N + ytop;
 y = repmat(y, 1, N);
 
-
+% Circle that bounds the 
 radius_bound = (x.^2 + y.^2 < params.radius^2);
 
 for i = 1:length(params.theta)
@@ -30,10 +42,7 @@ for i = 1:length(params.theta)
     w(:,[1:size(x,1)/2-params.radius,size(x,1)/2+params.radius:end]) =  0;
     exp_decay = exp(w./(params.D_p));
     
-    
     expProjContrib = interp2(x,y,exp_decay,t,t_perp,params.interp_method);
-%     expProjContrib(isnan(expProjContrib)) = 0;
-%     expProjContrib(expProjContrib >1) = 0;
 
     expProjContribNN = reshape(expProjContrib,N,N);
     expProjContribNN = expProjContribNN.*radius_bound;

a702095967ed27e7c443561febe09cd0ed418972
Joseph Toombs <jtoombs@berkeley.edu>
2021-08-29 01:37:17 -0700

    Work on PTB and setup

--- src/CALSetup.m
+++ src/CALSetup.m
@@ -13,25 +13,23 @@ classdef CALSetup
     
     methods (Static = true)
         function focus(dimensions)        
-            [X,Y] = meshgrid(linspace(0,dimensions(1)/2,dimensions(1)),linspace(0,dimensions(2)/2,dimensions(2)));
-            f_x = X./dimensions(1);
-            f_y = Y./dimensions(2);
-            grating_x = cos(2*pi*f_x)>0;
-            grating_y = cos(2*pi*f_y)>0;
+            [Y,X,~] = CALSetup.createGrid(dimensions);            
+            slices = 20;
+            rings = 0;
+            [theta,radius] = cart2pol(X,Y);
+            image = 1+sin(slices*theta);
+            image = double((image>1)&(radius<0.55)).*255;
 
-            A = zeros(dimensions(2),dimensions(1));
-            A(Y>X) = grating_y(Y>X);
-            A(Y<=X) = grating_x(Y<=X);
-            
-            figure;
-            imagesc(grating_x)
-            colormap('gray')
-            truesize
-%             CALProjectImage(image);
+            for ii = 1:rings
+                radius = 0.9*ii*1/(rings);
+                image(radius>(radius - 0.01) & radius<(radius + 0.01)) = 255;
+            end
+
+            CALProjectImage(image);
         end
         
-        function [image] = alignment(dimensions)
-            [X,Y,R] = CALSetup.createGrid(dimensions);
+        function [image] = opticalAlignment(dimensions)
+            [Y,X,R] = CALSetup.createGrid(dimensions);
             image = zeros(dimensions(2),dimensions(1));
             image(abs(X)<=0.005) = 255;
             image(abs(Y)<=0.005) = 255;
@@ -39,7 +37,64 @@ classdef CALSetup
             image(R<=0.4 & R>=0.39) = 255;
             image(R<=0.6 & R>=0.59) = 255;
             image(R<=0.8 & R>=0.79) = 255;
-%             CALProjectImage(image);
+            CALProjectImage(image);
+        end
+        
+        function [image,thickness,spacing] = axisAlignment(dimensions,varargin)
+            [~,X,~] = CALSetup.createGrid(dimensions);
+            
+            % convert all pixel dimensions to normalized dimensions
+            if nargin == 1
+                thickness = 5/dimensions(1);
+                spacing = 200/dimensions(1);             
+            elseif nargin == 2
+                thickness = varargin{1}/dimensions(1);
+                spacing = 200/dimensions(1);
+            elseif nargin == 3
+                thickness = varargin{1}/dimensions(1);
+                spacing = varargin{2}/dimensions(1);
+            end
+                
+            increment = 1/dimensions(1);
+            
+            image = createLines(thickness,spacing);
+
+            SLM = PTB();
+            SLM.drawImage(image);
+            
+                        
+            while true
+                pressed_key = PTB.checkKey();
+                if pressed_key == KbName('RightArrow')
+                    % increase spacing
+                    spacing = spacing + increment;
+                elseif pressed_key == KbName('LeftArrow')
+                    % decrease spacing
+                    spacing = spacing - increment;
+                elseif pressed_key == KbName('UpArrow')
+                    % increase thickness
+                    thickness = thickness + increment;
+                elseif pressed_key == KbName('DownArrow')
+                    % decrease thickness
+                    thickness = thickness - increment;
+                elseif pressed_key == KbName('ESCAPE')
+                    break
+                end
+                if thickness <= increment, thickness = 2*increment; end
+                if spacing <= 0, spacing = increment; end
+                
+                image = createLines(thickness,spacing);
+                SLM.drawImage(image);
+            end
+            
+            % convert final values to pixel dimensions
+            thickness = thickness*dimensions(1);
+            spacing = spacing*dimensions(1);
+            
+            function image = createLines(thickness,spacing)
+                image = zeros(dimensions(2),dimensions(1));
+                image(abs(X)>=spacing/2 & abs(X)<=spacing/2 + thickness) = 255;
+            end
         end
         
         function calibration()
@@ -48,7 +103,7 @@ classdef CALSetup
         
         function [image] = powerCalibration(dimensions,gray_value)
             image = ones(dimensions(2),dimensions(1))*gray_value;
-%             CALProjectImage(image);
+            CALProjectImage(image);
         end
         
         function [X,Y,R] = createGrid(dimensions)
@@ -59,37 +114,6 @@ classdef CALSetup
             R = sqrt(X.^2 + Y.^2);
         end
         
-%         function [gel_inds,void_inds] = getInds(target)
-%             circle_mask = CALMetrics.getCircleMask(target);
-%             gel_inds = circle_mask & target==1;
-%             void_inds = circle_mask & ~target;
-%         end
-%         
-%         function circle_mask = getCircleMask(target)
-%             target_dim = ndims(target);
-%             if target_dim == 2
-%                 [X,Y] = meshgrid(linspace(-1,1,size(target,1)),linspace(-1,1,size(target,1)));
-%                 R = sqrt(X.^2 + Y.^2);
-% 
-%             elseif target_dim == 3                
-%                 [X,Y,~] = meshgrid(linspace(-1,1,size(target,1)),linspace(-1,1,size(target,1)),linspace(-1,1,size(target,3)));
-%                 R = sqrt(X.^2 + Y.^2);
-%             end
-%             
-%             circle_mask = logical(R.*(R<=1));
-%         end
-%         
-%         function run(type,dimensions)
-%             if strcmp(type,'alignment')
-%                 image = CALSetup.alignment(dimensions);
-%             elseif strcmp(type,'focus')
-%                 image = CALSetup.focus(dimensions);
-%             elseif strcmp(type,'power')
-%                 varargin
-%                 image = CALSetup.powerCalibration(dimensions,
-%             end
-%             CALProjectImage(image);
-%         end
     end
 end
 
